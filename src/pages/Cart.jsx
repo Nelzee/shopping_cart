a468e6b3f9a3c8cd9e7ae1ca7243cd53adbd0a7d
Nelzee <nelzee07@gmail.com>
2024-08-12 22:12:45 +0200

    created the cart page

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Cart = () => {
+  return (
+    <>
+      <Navbar />
+      Cart
+    </>
+  );
+};
+
+export default Cart;
